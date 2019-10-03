2eb712c87d7d921f71c14ae975baf91c17964701
AdriDZea <49977661+AdriDZea@users.noreply.github.com>
2019-10-03 10:59:27 +0200

    Add files via upload

---
+++ 005_XML_StaX2_Git/src/com/tienda/modelo/Producto.java
@@ -0,0 +1,40 @@
+package com.tienda.modelo;
+
+public class Producto {
+	private String cliente;
+    private String nombre;
+    private String telefono;
+    private String comentarios;
+
+    
+	public String getCliente() {
+		return cliente;
+	}
+	public void setCliente(String cliente) {
+		this.cliente = cliente;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	public String getComentarios() {
+		return comentarios;
+	}
+	public void setComentarios(String comentarios) {
+		this.comentarios = comentarios;
+	}
+	
+	
+	@Override
+    public String toString() {
+        return "Cliente = " + cliente + ", Nombre = " + nombre + ", Teléfono = " + telefono + " Comentarios = " + comentarios;
+    }
+}
---
+++ 005_XML_StaX2_Git/src/com/tienda/vista/VerXML.java
@@ -0,0 +1,19 @@
+package com.tienda.vista;
+
+
+import java.util.List;
+
+import com.tienda.controlador.ParsearXML;
+import com.tienda.modelo.Producto;
+
+public class VerXML {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ParsearXML read = new ParsearXML();
+        List<Producto> readConfig = read.readConfig("C:\\adrian\\clientes.xml");
+        for (Producto item : readConfig) {
+            System.out.println(item);
+        }
+	}
+}
